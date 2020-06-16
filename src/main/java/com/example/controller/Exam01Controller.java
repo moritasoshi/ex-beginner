b3e2447bc8ca99729eb9337a55d928b3a0196965
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-06-16 10:54:33 +0900

    新規プロジェクトの作成

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("ex01")
+@RequestMapping("/ex01")
 public class Exam01Controller {
 	
 	@RequestMapping("")
@@ -13,7 +13,7 @@ public class Exam01Controller {
 		return "exam01";
 	}
 	
-	@RequestMapping("receive")
+	@RequestMapping("/receive")
 	public String receive(String name, Model model) {
 		model.addAttribute("name", name);
 		return "exam01-result";
