52bf469eb81bbe296d56ad29183457beceb06095
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-06-16 14:15:57 +0900

    演習5のhtmlファイルを作成

--- src/main/java/com/example/repository/MemberRepository.java
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -7,11 +7,13 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
 
 import com.example.domain.Member;
 
+@Repository
 public class MemberRepository {
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
